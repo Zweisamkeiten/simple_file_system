98828b234b5269544ab95b85a86dd66dae7da1fa
Einsam <snitxmhm@gmail.com>
2022-05-22 15:44:36 +0800

    fix: delete_node()

--- src/file_system.c
+++ src/file_system.c
@@ -105,6 +105,7 @@ void delete_node(NODE *cur_node) {
       delete_Helper(cur_node);
     } else {
       parent->child = cur_node->sibling;
+      cur_node->sibling = NULL;
       delete_Helper(cur_node);
     }
   } else {
